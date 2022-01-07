b8f98feefd21a0767f74723b673711d2d8591274
ronghaoZHI <895576961@qq.com>
2022-01-07 19:41:22 +0800

    Update generate.ts

--- src/core/generate.ts
+++ src/core/generate.ts
@@ -7,23 +7,7 @@ import { CallAppInstance } from '../index'
 import { logError } from '../libs/utils'
 import { genWXminiJumpPath } from '../libs/sdk'
 
-export const enum SchemeMapKeys {
-  HOME = 'home',
-  MSGCENTER = 'messagecenter',
-  MYBUY = 'mybuy',
-  PUBLISH = 'publish',
-  DETAIL = 'detail',
-  MYSELL = 'mysell',
-  ORDER = 'order',
-  PERSON = 'person',
-  VILLAGE = 'village',
-  WEB = 'web',
-}
 
-export interface UrlSearch {
-  openType: SchemeMapKeys
-  id?: string
-}
 export interface Intent {
   package: string
   scheme: string
@@ -56,7 +40,7 @@ export const generateScheme = (instance: CallAppInstance): string => {
 }
 
 // universal-link-host
-const universalLinkHost = 'mjump.zhuanzhuan.com'
+const universalLinkHost = ''
 
 // 生成 universalLink 链接
 export const generateUniversalLink = (instance: CallAppInstance) => {
@@ -72,11 +56,6 @@ export const generateUniversalLink = (instance: CallAppInstance) => {
   const path = targetInfo?.universalPath
   const channel = channelId ? `&channelId=${channelId}` : ''
 
-  // 中间下载页 标记位 // 目前只有 转转 找靓机支持 universal-link
-  // https://gitlab.zhuanspirit.com/zz-fe-common/app_download/-/blob/master/README.md
-  // app=zz 代表转转中间下载页
-  // app=zlj 代表找靓机中间下载页
-  // app=hunter 代表采货侠中间下载页
   let app = '&app=zz'
   if (targetInfo) {
     if (targetInfo.flag & AppFlags.ZZSeeker) {
@@ -119,109 +98,3 @@ export const generateIntent = (instance: CallAppInstance): string => {
   return `intent://${urlPath}${intentTail}`
 }
 
-// 兼容旧版本
-/**
- * 适配传统的吊起方式, 客户端后期将不会兼容openType的方式, 统一通过'zhuanzhuan://'的统跳形式拉起
- * (ps: 特殊客户端类似58App, 会采用schema和sdk结合的方式拉起App)
- *
- * 以前的入参:
- * 一个参数 { openType } :
- *      首页: { openType: 'home' }
- *      消息tab页: { openType: 'messagecenter' }
- *      我买到的: { openType: 'mybuy' }
- *      发布页: { openType: 'publish' }
- * 两个参数 { openType, id } :
- *      详情页: { openType: 'detail', id: 'infoId' }
- *      我卖出的: { openType: 'mysell', id: '' }
- *      订单详情页: { openType: 'order', id: 'orderId' }
- *      个人主页: { openType: 'person', id: 'userId' }
- *      小区页: { openType: 'village', id: 'villageId' }
- *      M页: { openType: 'web', id: 'url' }
- */
-
-/**
- * 跳转协议映射, 老的openType对应统跳的映射表
- * */
-
-export const SchemaMap = {
-  [SchemeMapKeys.HOME]: {
-    name: 'home',
-    path: 'zhuanzhuan://jump/core/mainPage/jump?tabId=0',
-    params: {
-      id: '',
-    },
-  },
-  [SchemeMapKeys.MSGCENTER]: {
-    name: 'messagecenter',
-    path: 'zhuanzhuan://jump/core/mainPage/jump?tabId=2',
-    params: {
-      id: '',
-    },
-  },
-  [SchemeMapKeys.MYBUY]: {
-    name: 'mybuy',
-    path: 'zhuanzhuan://jump/core/myBuyList/jump?tab=price',
-    params: {
-      id: '',
-    },
-  },
-  [SchemeMapKeys.PUBLISH]: {
-    name: 'publish',
-    path: 'zhuanzhuan://jump/core/publish/jump',
-    params: {
-      id: '',
-    },
-  },
-  [SchemeMapKeys.DETAIL]: {
-    name: 'detail',
-    path: 'zhuanzhuan://jump/core/infoDetail/jump',
-    params: {
-      id: 'infoId',
-    },
-  },
-  [SchemeMapKeys.MYSELL]: {
-    name: 'mysell',
-    path: 'zhuanzhuan://jump/core/mySellList/jump?tab=price',
-    params: {
-      id: '',
-    },
-  },
-  [SchemeMapKeys.ORDER]: {
-    name: 'order',
-    path: 'huanzhuan://jump/core/orderDetail/jump',
-    params: {
-      id: 'orderId',
-    },
-  },
-  [SchemeMapKeys.PERSON]: {
-    name: 'person',
-    path: 'zhuanzhuan://jump/core/personHome/jump',
-    params: {
-      id: 'uid',
-    },
-  },
-  [SchemeMapKeys.VILLAGE]: {
-    name: 'village',
-    path: 'zhuanzhuan://jump/core/village/jump',
-    params: {
-      id: 'villageId',
-    },
-  },
-  [SchemeMapKeys.WEB]: {
-    name: 'web',
-    path: 'zhuanzhuan://jump/core/web/jump',
-    params: {
-      id: 'url',
-    },
-  },
-}
-
-const getSchemeByUrlSearch = ({ openType = SchemeMapKeys.HOME, id = '' }: UrlSearch): string => {
-  const queryStr =
-    (id &&
-      `${!/\?/g.test(SchemaMap[openType]?.path) && '?'}${
-        SchemaMap[openType]?.params?.id
-      }=${encodeURIComponent(id)}`) ||
-    ''
-  return `${SchemaMap[openType]?.path}${queryStr}`
-}
