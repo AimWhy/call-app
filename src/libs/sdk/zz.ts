c92b866dc7782d9ba184947086603ef424f6cd0d
ronghaoZHI <895576961@qq.com>
2022-01-07 19:35:42 +0800

    Update zz.ts

--- src/libs/sdk/zz.ts
+++ src/libs/sdk/zz.ts
@@ -4,33 +4,12 @@ import { dependencies, SDKNames, wxInfo } from '../config'
 // import { checkOpen } from '../evoke'
 import { loadJSArr, logError, logInfo } from '../utils'
 
-// logInfo('enum SDKNames', SDKNames)
-/*
 
-可主动唤起的场景(需求)：
-
-｜ 目标app   ｜ 转转  ｜ 找靓机 ｜ 采货侠  ｜ 卖家版 ｜
-｜ 转转      ｜      ｜  ✅   ｜   ✅    ｜  ✅   ｜
-｜ 找靓机    ｜  ✅   ｜       ｜        ｜       ｜
-｜ 采货侠    ｜  ✅   ｜       ｜        ｜       ｜
-｜ 卖家版    ｜  ✅   ｜       ｜        ｜       ｜
-| zz-wx小程序|  ✅    |       ｜        ｜       ｜
-*/
-
-// 打开 转转内部 app
-// zz  zzSeller zzHunter 都走 zz-js-sdk + 统跳地址
-// 统跳地址平台： https://jump.zhuanspirit.com/#/zhuanzhuan?page=1&search=open
-
-// // 目前业务只需调起转转 wx-mini
 const miniprogramType = 0 // 默认小程序 0 正式版 / 1 开发版 / 2 体验版
-const zzWXMiniAppId = wxInfo.miniID //转转小程序 appid
-// 如果 targetAPP 是 wx小程序 并且 path 是页面地址，需要特殊处理(根据统跳协议地址自动拼接)
-export const genWXminiJumpPath = (path: string) =>
-  `zhuanzhuan://jump/core/miniProgram/jump?miniprogramType=${
-    miniprogramType || 0
-  }&path=${encodeURIComponent(path)}&userName=${zzWXMiniAppId}`
+const zzWXMiniAppId = wxInfo.miniID 
 
-// 打开转转/采货侠/卖家版 / 找靓机
+export const genWXminiJumpPath = (path: string) => ``
+
 const openAPP = (
   ctx: CallAppInstance,
   app: Record<string, any>,
@@ -99,8 +78,7 @@ export const loadZZSkd = (sdkName: SDKNames): Promise<Record<string, any>> => {
   return new Promise((resolve, reject) => {
     try {
       loadJSArr([dependencies[sdkName].link], () => {
-        //  确认 是否需要判断 找靓机 加载单独的 js-sdk
-        if (sdkName === SDKNames.ZZ_SDK) {
+        if (sdkName === SDKNames.Z_SDK) {
           logInfo('window.ZZAPP ==', window['@zz-common/zz-jssdk'].default)
           resolve(window['@zz-common/zz-jssdk'].default)
         }
@@ -110,14 +88,14 @@ export const loadZZSkd = (sdkName: SDKNames): Promise<Record<string, any>> => {
     }
   })
 }
-//  转转公司内部 app 都加载 zz-js-sdk
+
 export const openZZInnerApp = (
   ctx: CallAppInstance,
   curAppFlag: AppFlags,
   targetAppFlag: AppFlags
 ) => {
   const { callError = () => {} } = ctx.options
-  loadZZSkd(SDKNames.ZZ_SDK)
+  loadZZSkd(SDKNames.Z_SDK)
     .then((app) => {
       openAPP(ctx, app, curAppFlag, targetAppFlag)
     })
